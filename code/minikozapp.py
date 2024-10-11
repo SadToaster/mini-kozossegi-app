67a48e2f77b51ea53eee4fae91be0bf29ef727d8
SadToaster <matetunner6@gmail.com>
2024-10-11 13:53:45 +0200

    funkciók

---
+++ code/minikozapp.py
@@ -0,0 +1,72 @@
+from email import message
+import tkinter as tk
+from tkinter import ttk
+
+
+def posztolas():
+
+    post = tk.Toplevel()
+    post.title("Posztolás")
+    post.config(width=300, height=200)
+
+    button_close = ttk.Button(
+        post,
+        text="Close window",
+        command=post.destroy
+    )
+    button_close.place(x=75, y=75)
+
+def uzi():
+
+    message = tk.Toplevel()
+    message.title("Üzenetváltás")
+    message.config(width=300, height=200)
+
+    button_close = ttk.Button(
+        message,
+        text="Close window",
+        command=message.destroy
+    )
+    button_close.place(x=75, y=75)
+
+def tarplat():
+
+    tartalom = tk.Toplevel()
+    tartalom.title("Fórum")
+    tartalom.config(width=300, height=200)
+
+    button_close = ttk.Button(
+        tartalom,
+        text="Close window",
+        command=tartalom.destroy
+    )
+    button_close.place(x=75, y=75)
+
+
+# Create the main window.
+felhasznalasi_lehetosegek = tk.Tk()
+felhasznalasi_lehetosegek.config(width=500, height=500)
+felhasznalasi_lehetosegek.title("Felhasználási lehetőségek")
+# Create a button inside the main window that
+# invokes the posztolas() function
+# when pressed.
+poszt = ttk.Button(
+    felhasznalasi_lehetosegek,
+    text="Posztolás",
+    command=posztolas
+)
+poszt.place(x=100, y=100)
+uzengetes = ttk.Button(
+    felhasznalasi_lehetosegek,
+    text="Üzengetés",
+    command=uzi
+)
+uzengetes.place(x=100, y=200)
+
+tartalom_platform = ttk.Button(
+    felhasznalasi_lehetosegek,
+    text="Fórum",
+    command=tarplat
+)
+tartalom_platform.place(x=100, y=300)
+felhasznalasi_lehetosegek.mainloop()
