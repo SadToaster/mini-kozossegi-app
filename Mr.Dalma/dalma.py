2b8b74c189ce17316909bc76af490284eb55a48b
NagyLevi12 <levinagy1207@gmail.com>
2024-10-18 12:49:47 +0200

    asd

--- Mr.Dalma/dalma.py
+++ Mr.Dalma/dalma.py
@@ -5,33 +5,40 @@ from tkinter.ttk import Frame, Label, Style
 root = Tk()
 root.title("Mini-közösségi app")
 root.geometry("800x400")
+root.configure(background="cadetblue")
 
-felhasznalo = Entry(root, width=50, bg="cadet blue", fg="white", borderwidth=10)
+felhasznalo = Entry(root, width=50, bg="white", fg="black", borderwidth=10)
 felhasznalo.insert(0, "")
-felhasznalo.place(relx=0.25,rely=0.1,anchor=N)
+felhasznalo.place(relx=0.5,rely=0.1,anchor=N)
 
-jelszo= Entry(root, width=50, bg="cadet blue",fg="white", borderwidth=10)
+jelszo= Entry(root, width=50, bg="white",fg="black", borderwidth=10)
 jelszo.insert(0, "")
-jelszo.place(relx=0.7,rely=0.1,anchor=N)
+jelszo.place(relx=0.5,rely=0.25,anchor=N)
+
+bejeadatok = []
+with open('./bejelentkezes.txt','r', encoding='utf-8')as file:
+    for i in file:
+        adat = i.strip().split(';')
+        bejeadatok.append({
+            'felnev' : str(adat[0]),
+            'jelszo' : str(adat[1])
+        })
+
 
 def bejelentkezes():
     global felhasznalo
     global jelszo
-    if felhasznalo == "Levi69420" and jelszo == "BoldogtalanvagyokLOL10":
-        print("asd")
-    elif felhasznalo == "Máté69421" and jelszo == "BoldogtalanvagyokLOL11":
-        print("asd")
+    global bejeadatok
+    for i in bejeadatok:
+        if felhasznalo == i['felnev'] and jelszo == i['jelszo']:
+            print('asd')
 
 bejelentkezes()
 
-button = Button(root, text="Bejelentkezés", padx=10, pady=10, command=bejelentkezes, fg="white", bg="cadetblue")
-button.place(relx=0.5, rely=0.25, anchor=N)
-btn2 = Button(root, text="Bezárás",padx=10,pady=10, command=root.destroy)
-btn2.place(relx=0.5,rely=0.75, anchor=N)
-
-
-
-
+button = Button(root, text="Bejelentkezés", padx=10, pady=10, command=bejelentkezes, fg="black", bg="white")
+button.place(relx=0.5, rely=0.5, anchor=N)
+btn2 = Button(root, text="X",padx=10,pady=10,fg="red",bg="white" ,command=root.destroy)
+btn2.place(relx=0.5,rely=0.7, anchor=N)
 
 
 root.mainloop()
