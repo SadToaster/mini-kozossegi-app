6f5509f276b35ca61a2e098bc2bff85660faea89
NagyLevi12 <levinagy1207@gmail.com>
2024-10-29 12:54:53 +0100

    fasz

--- code/levi/fal.py
+++ code/levi/fal.py
@@ -4,6 +4,8 @@ from tkinter.ttk import Frame, Label, Style
 
 
 def open_window(felnev):
+    global entri
+    global posz
     root = Tk()
     root.title("Közösségi fal")
     root.geometry("800x600")
@@ -15,8 +17,10 @@ def open_window(felnev):
     entri.place(relx=0.25,rely=0.7,anchor=N)
     validalo = Button(root,text="Posztolás",padx=10, pady=10,command=posztolas)
     validalo.place(relx=0.1,rely=0.8,anchor=N)
-    def posztolas():
-        getter = entri.get()
-        posz.config(text=getter)
-    
+def posztolas():
+    global entri
+    global posz
+    getter = entri.get()
+    posz = Label(text=getter)
 
+open_window("asd")
