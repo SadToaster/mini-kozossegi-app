12d99165870aaf3e783a2260e11bd360ec168449
NagyLevi12 <levinagy1207@gmail.com>
2024-11-03 12:06:40 +0100

    ennyi

--- code/fal.py
+++ code/fal.py
@@ -2,6 +2,8 @@ from tkinter import*
 from tkinter import Tk, BOTH
 from tkinter.ttk import Frame, Label, Style
 import profil
+import tkinter as tk
+from tkinter import ttk
 
 
 def open_window(felnev):
@@ -11,34 +13,38 @@ def open_window(felnev):
     root.title("Közösségi fal")
     root.geometry("800x600")
     root.configure(background="cadetblue")
-    posz = Label(root,text="")
-    posz.place(relx=0.1,rely=0.1,anchor=N)
     entri = Entry(root, width=50, bg="white", fg="black", borderwidth=10)
     entri.insert(0,"Mi jár a fejedben?")
+    entri.place(relx=0.25,rely=0.7,anchor=N)
     def posztolas():
         global entri
         global posz
         getter = entri.get()
         post = []
-        with open("./mini-kozossegi-app/code/fal.txt","a",encoding="utf-8")as file:
+        posz = tk.Label(root, text="Posztolás:")
+        posz.place(relx=0.1,rely=10000,anchor=N)
+        with open("./code/fal.txt","a",encoding="utf-8")as file:
             print(f"{felnev}\n{getter}",file=file)
-        with open("./mini-kozossegi-app/code/fal.txt","r",encoding="utf-8")as fajl:
+        with open("./code/fal.txt","r",encoding="utf-8")as fajl:
             for i in fajl:
                 post.append(
                     i
                 )
-        print(post)
         kiiratas=str(post)
         kiiratas=kiiratas.replace("[","")
         kiiratas=kiiratas.replace("]","")
-        posz.config(text='\n'.join(map(str, post)))
-    entri.place(relx=0.25,rely=0.7,anchor=N)
+        def display_range(a, b):
+            posz["text"] = f"Range: {a}-{b}"
+        listbox = tk.Listbox(yscrollcommand=display_range, width=80)
+        listbox.insert(tk.END, *(post[i] for i in range(len(post))))
+        listbox.place(rely=0.4, relx=0.05, anchor=W, height=250)
     validalo = Button(root,text="Posztolás",padx=10, pady=10,command=posztolas)
     validalo.place(relx=0.1,rely=0.8,anchor=N)
     def prgab():
         profil.profil_galeria()
     gab = Button(root,text="Profil/galéria",command=prgab,padx=10,pady=10,fg="black", bg="white")
     gab.place(relx=0.8,rely=0.1,anchor=N)
+    
 
 
 open_window("asd")
