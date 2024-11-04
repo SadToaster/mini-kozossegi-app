46b75ef6b603c4970711d82de35be0ade6b1477f
NagyLevi12 <levinagy1207@gmail.com>
2024-11-04 21:31:00 +0100

    faszulás

--- code/proba.py
+++ code/proba.py
@@ -1,21 +1,26 @@
 from tkinter import *
 from PIL import ImageTk, Image
-# set up the tkinter window
+
 root = Tk()
 root.title("Profil/Galéria")
-root.geometry("1000x800")
+root.geometry("1100x750")
+root.configure(background="cadetblue")
 
-# set up the images
-image1 = ImageTk.PhotoImage(Image.open("./code/img/levi1.jpg").resize((600, 350)))
-image2 = ImageTk.PhotoImage(Image.open("./code/img/tunner1.jpg").resize((600, 350)))
-image3 = ImageTk.PhotoImage(Image.open("./code/img/tunner2.jpg").resize((600, 350)))
-#image4 = ImageTk.PhotoImage(Image.open("images/04.jpg").resize((600, 350)))
-#image5 = ImageTk.PhotoImage(Image.open("images/05.jpg").resize((600, 350)))
-# add them to the list
-image_list = [image1, image2, image3]
-# counter integer
+tunnerlabel = Label(root,text="Tunner Máté Albert")
+tunnerlabel.place(relx=0.32,rely=0.02,anchor=N)
+tunnerbemutatkozas = Label(root, text="Tunner Máté (alias Mr. szomorú)\n-a Veszprém SZC Ipari Technikumba jár informatika szakra\n-Hobbija az aktív sportolás és az olvasás.\n-Szereti: a könnyed, de nem üres beszédet\n-Nem szereti: a hivalkodó embereket",justify=LEFT)
+tunnerbemutatkozas.place(relx=0.8,rely=0.21,anchor=N)
+
+tunner1 = ImageTk.PhotoImage(Image.open("./code/img/tunner8.jpg").resize((290, 290)))
+tunner2 = ImageTk.PhotoImage(Image.open("./code/img/tunner2.jpg").resize((290, 290)))
+tunner3 = ImageTk.PhotoImage(Image.open("./code/img/tunner6.jpg").resize((290, 290)))
+tunner4 = ImageTk.PhotoImage(Image.open("./code/img/tunner3.jpg").resize((290, 290)))
+tunner5 = ImageTk.PhotoImage(Image.open("./code/img/tunner4.jpg").resize((290, 290)))
+
+image_list = [tunner1, tunner2, tunner3, tunner4, tunner5]
+
 counter = 0
-# change image function
+
 def forwChangeImage():
     global counter
     if abs(counter) < len(image_list) - 1:
@@ -23,7 +28,7 @@ def forwChangeImage():
     else:
         counter = 0
     imageLabel.config(image=image_list[counter])
-    #infoLabel.config(text="Image " + str(counter + 1) + " of " + str(len(image_list)))
+    
 def backChangeImage():
     global counter
     if abs(counter) < len(image_list) - 1:
@@ -31,17 +36,52 @@ def backChangeImage():
     else:
         counter = 0
     imageLabel.config(image=image_list[counter])
-    #infoLabel.config(text="Image " + str(counter + 1) + " of " + str(len(image_list)))
+    
 
-# set up the components
-imageLabel = Label(root, image=image1)
-#infoLabel = Label(root, text="Image 1 of 3", font="Helvetica, 20")
-button1 = Button(root, text="next", width=20, height=2, bg="purple", fg="white", command=forwChangeImage)
-button2 = Button(root, text="previous", width=20, height=2, bg="purple", fg="white", command=backChangeImage)
-# display the components
-imageLabel.pack()
-#infoLabel.pack()
-button1.pack(side=RIGHT, pady=3)
-button2.pack(side=LEFT, pady=3)
-# run the main loop
+imageLabel = Label(root, image=tunner1)
+imageLabel.place(relx=0.32,rely=0.06,anchor=N)
+next1 = Button(root, text="következő", width=20, height=2, bg="white", fg="black", command=forwChangeImage)
+previous1 = Button(root, text="előző", width=20, height=2, bg="white", fg="black", command=backChangeImage)
+next1.place(relx=0.09,rely=0.25,anchor=N)
+previous1.place(relx=0.55,rely=0.25,anchor=N)
+
+levilabel = Label(root,text="Nagy Levente")
+levilabel.place(relx=0.32,rely=0.5,anchor=N)
+levibemutatkozás = Label(root,text="Nagy Levente (alias Mr. Dalma)\n-a Veszprém SZC Ipari Technikumba jár informatika szakra\n-Hobbija a biciklizés, futás, túrázás\n-Szereti: sporttal töltött időt\n-Nem szereti:káposzta" ,justify=LEFT)
+levibemutatkozás.place(relx=0.8,rely=0.67,anchor=N)
+
+levi1 = ImageTk.PhotoImage(Image.open("./code/img/levi2.jpg").resize((290, 290)))
+levi2 = ImageTk.PhotoImage(Image.open("./code/img/levi1.jpg").resize((290, 290)))
+levi3 = ImageTk.PhotoImage(Image.open("./code/img/levi3.jpg").resize((290, 290)))
+levi4 = ImageTk.PhotoImage(Image.open("./code/img/levi4.jpg").resize((290, 290)))
+levi5 = ImageTk.PhotoImage(Image.open("./code/img/levi5.jpg").resize((290, 290)))
+
+image_listlevi = [levi1, levi2, levi3, levi4, levi5]
+
+counterlevi = 0
+
+def forwChangeImagelevi():
+    global counterlevi
+    if abs(counterlevi) < len(image_listlevi) - 1:
+        counterlevi += 1
+    else:
+        counterlevi = 0
+    imageLabellevi.config(image=image_listlevi[counterlevi])
+    
+def backChangeImagelevi():
+    global counterlevi
+    if abs(counterlevi) < len(image_listlevi) - 1:
+        counterlevi -= 1
+    else:
+        counterlevi = 0
+    imageLabellevi.config(image=image_listlevi[counterlevi])
+    
+
+imageLabellevi = Label(root, image=levi2)
+imageLabellevi.place(relx=0.32,rely=0.54,anchor=N)
+next2 = Button(root, text="következő", width=20, height=2, bg="white", fg="black", command=forwChangeImagelevi)
+previous2 = Button(root, text="előző", width=20, height=2, bg="white", fg="black", command=backChangeImagelevi)
+next2.place(relx=0.09,rely=0.7,anchor=N)
+previous2.place(relx=0.55,rely=0.7,anchor=N)
+
 root.mainloop()
