edf68015ce5f18310aa2972719ed34e384819a3c
NagyLevi12 <levinagy1207@gmail.com>
2024-11-04 06:31:35 +0100

    siker!!!!!!!sikerult a slide show

--- code/proba.py
+++ code/proba.py
@@ -1,78 +1,47 @@
-from tkinter import Tk
+from tkinter import *
 from PIL import ImageTk, Image
-from tkinter import Tk, BOTH
-from tkinter.ttk import Frame, Label, Style
-
-# Create a Tkinter root window
+# set up the tkinter window
 root = Tk()
-root.geometry('1000x900')
-root.title("Slider")
+root.title("Profil/Galéria")
+root.geometry("1000x800")
 
-# List of image file paths
-pics_list = ["./code/img/levi1.jpg",
-             "./code/img/tunner1.jpg",
-             "./code/img/tunner2.jpg",
-             ]
+# set up the images
+image1 = ImageTk.PhotoImage(Image.open("./code/img/levi1.jpg").resize((600, 350)))
+image2 = ImageTk.PhotoImage(Image.open("./code/img/tunner1.jpg").resize((600, 350)))
+image3 = ImageTk.PhotoImage(Image.open("./code/img/tunner2.jpg").resize((600, 350)))
+#image4 = ImageTk.PhotoImage(Image.open("images/04.jpg").resize((600, 350)))
+#image5 = ImageTk.PhotoImage(Image.open("images/05.jpg").resize((600, 350)))
+# add them to the list
+image_list = [image1, image2, image3]
+# counter integer
+counter = 0
+# change image function
+def forwChangeImage():
+    global counter
+    if abs(counter) < len(image_list) - 1:
+        counter += 1
+    else:
+        counter = 0
+    imageLabel.config(image=image_list[counter])
+    #infoLabel.config(text="Image " + str(counter + 1) + " of " + str(len(image_list)))
+def backChangeImage():
+    global counter
+    if abs(counter) < len(image_list) - 1:
+        counter -= 1
+    else:
+        counter = 0
+    imageLabel.config(image=image_list[counter])
+    #infoLabel.config(text="Image " + str(counter + 1) + " of " + str(len(image_list)))
 
-levinev = Label(root,text="Nagy Levente")
-levinev.place(relx=0.5,rely=0.5,anchor=N)
-
-# Create labels for displaying image and navigation buttons
-lbl_next = Label(text=">", font=("Verdana",25))
-lbl_previous = Label(text="<", font=("Verdana",25))
-lbl_picture = Label()
-lbl_picture.place(relx=0.5,rely=0.5,anchor=N)
-
-# Grid layout for labels
-lbl_previous.grid(row=0, column=0)
-lbl_picture.grid(row=0, column=1)
-lbl_next.grid(row=0, column=2)
-
-# Initialize the index to track the current image
-index = 0
-
-def nextImage():
-    global index, img
-
-    # Increment the index to show the next image in the list
-    index += 1
- 
-    # If the index goes beyond the last image, loop back to the first image
-    if index > len(pics_list) - 1:
-        index = 0
-
-    # Load and display the image at the current index
-    img = Image.open(pics_list[index])
-    img = img.resize((300,370))
-    img = ImageTk.PhotoImage(img)
-    lbl_picture['image'] = img
-
-
-
-def previousImage():
-    global index, img
-
-    # Decrement the index to show the previous image in the list
-    index -= 1
-
-    # If the index goes below 0, loop back to the last image
-    if index < 0:
-        index =  len(pics_list) - 1
-
-    # Load and display the image at the current index
-    img = Image.open(pics_list[index])
-    img = img.resize((330,400))
-    img = ImageTk.PhotoImage(img)
-    lbl_picture['image'] = img
-
-
-# Bind the navigation buttons to their respective functions
-lbl_next.bind("<Button-1>", lambda abc : nextImage())
-lbl_previous.bind("<Button-1>", lambda abc : previousImage())
-
-
-# Show the first image in the list when the program starts
-nextImage()
-
-# Start the Tkinter event loop
+# set up the components
+imageLabel = Label(root, image=image1)
+#infoLabel = Label(root, text="Image 1 of 3", font="Helvetica, 20")
+button1 = Button(root, text="next", width=20, height=2, bg="purple", fg="white", command=forwChangeImage)
+button2 = Button(root, text="previous", width=20, height=2, bg="purple", fg="white", command=backChangeImage)
+# display the components
+imageLabel.pack()
+#infoLabel.pack()
+button1.pack(side=RIGHT, pady=3)
+button2.pack(side=LEFT, pady=3)
+# run the main loop
 root.mainloop()
